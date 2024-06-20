0f018b5aeba602ece2b76dd5bdc6eb398dd4397a
Raziel619 <raziel619dev@gmail.com>
2024-06-20 10:13:14 -0400

    Update default to 10s

--- process_folders.py
+++ process_folders.py
@@ -8,6 +8,10 @@ if __name__ == "__main__":
     dirs = [f.path for f in os.scandir(input_dir) if f.is_dir()]
 
     for x in dirs:
+        file_count = len([f for f in os.listdir(x) if os.path.isfile(os.path.join(x, f))])
+        if "_ls" in x or file_count > 1:
+            continue
+
         print(f"Processing - {x}")
         start_time = time.time()
         subprocess.run([sys.executable, f"main.py", "--src_folder", x, "--video_folder", x])
